aa6c4994a5e8145fd1ca2d6bf77b6fafe285654d
dutdut77 <dutdut77@hotmail.fr>
2026-01-13 22:52:15 +0100

    table slideover calendar tache

--- app/pages/calendriers/taches.vue
+++ app/pages/calendriers/taches.vue
@@ -293,7 +293,8 @@ const listeTachesDecembre = computed(() => {
 </script>
 
 <template>
-  <section class="flex w-full flex-col gap-4 p-4 lg:h-full lg:px-4 lg:py-0 lg:pt-4">
+  <section
+    class="flex w-full flex-col gap-4 p-4 lg:h-full lg:overflow-hidden lg:px-4 lg:py-0 lg:pt-4 print:overflow-visible">
     <div class="flex w-full flex-col gap-4 md:flex-row md:items-center">
       <div class="flex flex-col gap-4 sm:flex-row sm:items-center sm:justify-between print:justify-start">
         <img src="/images/logo_uo.png" alt="Logo" class="hidden w-12 print:block" />
@@ -371,10 +372,15 @@ const listeTachesDecembre = computed(() => {
               </div>
             </div>
 
-            <div class="mt-6 flex w-full overflow-auto pb-4 text-sm print:hidden">
-              <table class="relative min-w-full bg-white">
+            <div class="mt-4 w-full pb-4 text-sm print:hidden">
+              <table class="relative w-full bg-white" style="table-layout: fixed">
+                <colgroup>
+                  <col style="width: 48px" />
+                  <col style="width: 120px" />
+                  <col />
+                </colgroup>
                 <thead class="">
-                  <tr class="sticky top-0 h-16 bg-white tracking-wider">
+                  <tr class="h-16 bg-white tracking-wider">
                     <th class="border-b-2 border-gray-300 px-4 py-3 text-left leading-4 tracking-wider">#</th>
                     <th class="border-b-2 border-gray-300 px-4 py-3 text-left leading-4 tracking-wider">Catégories</th>
                     <th class="border-b-2 border-gray-300 px-4 py-3 text-left leading-4 tracking-wider">Tâches</th>
@@ -385,7 +391,7 @@ const listeTachesDecembre = computed(() => {
                   :key="index"
                   class="cursor-default odd:bg-white even:bg-slate-50 hover:bg-slate-100">
                   <tr>
-                    <td class="whitespace-no-wrap w-12 border-b py-2 pl-4 print:hidden" @click.stop>
+                    <td class="border-b py-2 pl-4 print:hidden" @click.stop>
                       <div>
                         <input
                           type="checkbox"
@@ -412,13 +418,11 @@ const listeTachesDecembre = computed(() => {
                         </label>
                       </div>
                     </td>
-                    <td class="whitespace-no-wrap border-b px-4 py-2">
-                      <div class="flex items-center">
-                        <div class="leading-5">{{ tache.categorie }}</div>
-                      </div>
+                    <td class="border-b px-4 py-2">
+                      <div class="leading-5 break-words">{{ tache.categorie }}</div>
                     </td>
-                    <td class="whitespace-no-wrap border-b p-4">
-                      <div class="leading-5">{{ tache.tache }}</div>
+                    <td class="border-b p-4">
+                      <div class="break-words">{{ tache.tache }}</div>
                     </td>
                   </tr>
                 </tbody>
@@ -437,3 +441,17 @@ const listeTachesDecembre = computed(() => {
     </AppSlideOver>
   </section>
 </template>
+
+<style scoped>
+@media print {
+  * {
+    -webkit-print-color-adjust: exact !important;
+    print-color-adjust: exact !important;
+  }
+
+  @page {
+    size: A3 landscape;
+    margin: 5mm;
+  }
+}
+</style>
